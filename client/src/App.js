4e0dda391b2235c7996595a652cad0d01065860f
Aleks <Aleksei.developer@yandex.ru>
2023-03-21 20:56:57 +0300

    add client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import PostList from "./components/PostList";
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <NavBar />
+        <PostList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PostItem from "./PostItem";
+
+const PostList = () => {
+    return (
+        <div>
+            Posts:
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+        </div>
+    );
+};
+
+export default PostList;
